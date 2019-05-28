9e1994cb657baceeedb8c00df3cb7ef6aca2a831
Sofia Wännerdahl <sofiawannerdahl@hotmail.com>
2019-05-28 23:53:38 +0200

    created a component for header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+  render(){
+    return(
+      <header>
+        <h1> Programming jokes <h1/>
+      <header/>
+    )
+  }
+}
