958e4efb2ff934f21cb0f47855da3e54d819d46a
Hanna <fellwinghanna@gmail.com>
2019-05-31 20:31:04 +0200

    add every joke in localStorage and get function works, but make dublicates with onchange method

--- src/App.js
+++ src/App.js
@@ -18,14 +18,15 @@ class App extends Component{
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+
     handleChange(event) {
         this.setState({category: event.target.value});
     }
 
     handleSubmit(event) {
-     //alert(this.state.category);
      event.preventDefault();
      let category = this.state.category;
+     console.log(category)
 
      var url;
      if (category === "All"){
@@ -51,6 +52,7 @@ class App extends Component{
     
     render(){
         const jokes = this.state.jokelist;
+        console.log(jokes)
         return (
             <div>
                 <Header/>
--- src/Joke.js
+++ src/Joke.js
@@ -4,18 +4,30 @@ export class Joke extends Component {
 
     render() {
         const jokes = this.props.joke;
-        console.log(jokes);
+        const J = JSON.parse(localStorage.getItem("joke"));
+        console.log(J);
         //const jokeitem = <p>{jokes} </p>;
 
-            return (
+        if(J.length === 0){
+            return(
+            <div id="joke-container">
+                <div id="joke">
+                    <p>{jokes} </p>
+                </div>
+            </div>
+        )}else{
+            return(
                 <div id="joke-container">
-                    <div id="joke">
-                        <p>
-                            {jokes}
-                        </p>
-                     </div>
+                    {J.map((text, i) => {
+                        return(
+                        <div key={i} id="joke">
+                            <p>{text.text}</p>
+                        </div>
+                        )
+                    })}
                 </div>
             )
+        }
     }
 }
 
